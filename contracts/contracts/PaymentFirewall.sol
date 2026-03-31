b2fc5844a114302431dca30a68846d2ecac98eac
Jean-Christophe Rona <jc@rona.fr>
2026-03-31 15:00:22 +0200

    Improve security of example contract
    - add reentrancy protection
    - add some checks on input arguments

--- contracts/contracts/PaymentFirewall.sol
+++ contracts/contracts/PaymentFirewall.sol
@@ -1,18 +1,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8;
 
+import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import {Trustlined} from "@trustline.id/evmsdk/contracts/Trustlined.sol";
 
 /// @title PaymentFirewall
 /// @author Trustline
 /// @notice This contract is a firewall ensuring all funds going out are sent by a verified user
-contract PaymentFirewall is Trustlined {
+contract PaymentFirewall is Trustlined, ReentrancyGuard {
     constructor(address trustlineValidationEngineLogic, address trustlineValidationEngineProxy) Trustlined(trustlineValidationEngineLogic, trustlineValidationEngineProxy) {}
 
     /// @notice Pay native ethers to a recipient
     /// @param dest the destination address of the funds
-    function payEthers(address payable dest) public payable {
-    
+    function payEthers(address payable dest) public payable nonReentrant {
+        require(dest != address(0), "Invalid destination");
+        require(msg.value > 0, "Invalid amount");
+
         // the line for protecting this method
         requireTrustline();
 
